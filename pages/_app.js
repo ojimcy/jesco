2bd0daec57a09d7d95b72bac76d1cae6529845d3
ojimcy <ojimcy247@gmail.com>
2023-01-18 19:27:10 +0100

    create list of products

--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,4 @@
+import '@/styles/globals.css'
 import { useEffect } from 'react'
 
 export default function App({ Component, pageProps }) {
--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,43 @@
 import Layout from '@/components/Layout'
+import data from '@/utils/data'
+import {
+  Button,
+  Card,
+  CardActionArea,
+  CardActions,
+  CardContent,
+  CardMedia,
+  Grid,
+  Typography,
+} from '@material-ui/core'
 
 export default function Home() {
   return (
     <Layout>
       <div>
         <h1>Products</h1>
-        <ul>
-          <li>Products 1</li>
-          <li>Products 2</li>
-          <li>Products 3</li>
-        </ul>
+        <Grid container spacing={3}>
+          {data.products.map((product) => (
+            <Grid item md={4} key={product.name}>
+              <Card>
+                <CardActionArea>
+                  <CardMedia
+                    component="img"
+                    image={product.image}
+                    title={product.name}
+                  ></CardMedia>
+                  <CardContent>
+                    <Typography>{product.name}</Typography>
+                  </CardContent>
+                </CardActionArea>
+                <CardActions>
+                  <Typography>${product.price}</Typography>
+                  <Button size='small' color='primary'>Add to cart</Button>
+                </CardActions>
+              </Card>
+            </Grid>
+          ))}
+        </Grid>
       </div>
     </Layout>
   )
