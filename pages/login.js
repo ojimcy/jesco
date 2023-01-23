0e8d6e2b31d2f8e6ae4315ee9f8d31e9b6d40a57
ojimcy <ojimcy247@gmail.com>
2023-01-23 12:38:19 +0100

    login

--- pages/login.js
+++ pages/login.js
@@ -8,14 +8,30 @@ import {
   TextField,
   Typography,
 } from '@material-ui/core';
+import axios from 'axios';
 import NextLink from 'next/link';
-import React from 'react';
+import React, { useState } from 'react';
 
 export default function Login() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
   const classes = useStyles();
+
+  const submitHandler = async (e) => {
+    e.preventDefault();
+    try {
+      const { data } = await axios.post('/api/users/login', {
+        email,
+        password,
+      });
+      alert('Logged successfully!')
+    } catch (err) {
+      alert(err.response.data ? err.response.data.message : err.message);
+    }
+  };
   return (
     <Layout title="Login">
-      <form className={classes.form}>
+      <form onSubmit={submitHandler} className={classes.form}>
         <Typography component="h1" variant="h1">
           Login
         </Typography>
@@ -27,6 +43,7 @@ export default function Login() {
               id="email"
               label="Email"
               inputProps={{ type: 'email' }}
+              onChange={(e) => setEmail(e.target.value)}
             ></TextField>
           </ListItem>
           <ListItem>
@@ -36,6 +53,7 @@ export default function Login() {
               id="password"
               label="Password"
               inputProps={{ type: 'password' }}
+              onChange={(e) => setPassword(e.target.password)}
             ></TextField>
           </ListItem>
           <ListItem>
@@ -44,7 +62,7 @@ export default function Login() {
             </Button>
           </ListItem>
           <ListItem>
-            Don't have an accout? &nbsp;s
+            Don't have an accout? &nbsp;
             <NextLink href="/register" passHref>
               <Link>Register</Link>
             </NextLink>
