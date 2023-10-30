2dc2f8d1bccaf03006020384c66e5e84bf263c88
ojimcy <ojimcy247@gmail.com>
2023-10-30 14:13:40 +0100

    Edit user

---
+++ pages/api/users/[id]/index.js
@@ -0,0 +1,32 @@
+import db from '@/utils/db';
+import User from '@/utils/models/User';
+import { createRouter } from 'next-connect';
+import { isAdmin, isAuth } from '@/utils/auth';
+
+const router = createRouter();
+router.use(isAuth, isAdmin);
+
+router.put(async (req, res) => {
+  await db.connect();
+   const user = await User.findById(req.query.id);
+   if (user) {
+     user.name = req.body.name;
+     user.slug = req.body.slug;
+     user.price = req.body.price;
+     user.category = req.body.category;
+     user.image = req.body.image;
+     user.brand = req.body.brand;
+     user.countInStock = req.body.countInStock;
+     user.description = req.body.description;
+     user.isAdmin = Boolean(req.body.isAdmin);
+     await user.save();
+     await db.disconnect();
+     res.send({ message: 'User Updated Successfully' });
+   } else {
+     await db.disconnect();
+     res.status(404).send({ message: 'User Not Found' });
+   }
+});
+
+
+export default router.handler();
